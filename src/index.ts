66a0f47b38bb6be123aead050b475dfe502de886
Chris Paterson <cpaterson@conversantmedia.com>
2019-05-21 12:10:01 -0700

    Forgot to add index

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import CmpApi from './cmpApi';
+import GVL from './GVL';
+import TCString from './tcString';
+import TCModel from './tcString/model/TCModel';
+import CMPManifest from './tcString/model/CMPManifest';
+
+
+export {CmpApi, GVL, TCString, TCModel, CMPManifest};
