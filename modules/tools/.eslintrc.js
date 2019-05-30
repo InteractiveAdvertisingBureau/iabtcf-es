46b65385b0ccdd79bf5e6c0322d35425f6e20281
Chris Paterson <cpaterson@conversantmedia.com>
2019-05-30 10:25:25 -0700

    Adding scaffold for vue tools project

---
+++ modules/tools/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: [
+    // add more generic rulesets here, such as:
+    // 'eslint:recommended',
+    'plugin:vue/recommended'
+  ],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  }
+}
+
