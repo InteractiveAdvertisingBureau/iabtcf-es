0763753b80307400931e0b1327678a11c386038e
Chris Paterson <cpaterson@conversantmedia.com>
2019-10-26 19:05:38 -0700

    Moving to root level

--- modules/tools/vue.config.js
+++ modules/tools/vue.config.js
@@ -2,7 +2,7 @@ const path = require('path');// eslint-disable-line
 
 module.exports = {
   lintOnSave: false,
-  outputDir: path.resolve(__dirname, '../../docs/tools/'),
+  outputDir: path.resolve(__dirname, '../../docs/'),
   configureWebpack: {
     devServer: {
       contentBase: [path.join(__dirname, 'dist'), path.join(__dirname, 'vendorlist')],
