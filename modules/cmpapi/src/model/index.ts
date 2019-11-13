b4aff5f54eb6f27224fa2fd9d42ec0f29f5e89e7
Shane Swayney <sswayney@conversantmedia.com>
2019-11-13 10:33:09 -0800

    Small review change: Export fix.

--- modules/cmpapi/src/model/index.ts
+++ modules/cmpapi/src/model/index.ts
@@ -4,4 +4,4 @@ export * from './returned/Ping';
 export * from './returned/GlobalVendorList';
 export * from './Restrictions';
 export * from './BooleanVector';
-export {StringBoolVector} from "./StringBoolVector";
+export * from './StringBoolVector';
