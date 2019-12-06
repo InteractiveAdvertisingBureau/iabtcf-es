a80e0e60598443e9b9cf57eb6497f5d5842f99ea
Chris Paterson <1268493+chrispaterson@users.noreply.github.com>
2019-12-06 15:28:03 -0800

    working on site

--- modules/core/src/index.ts
+++ modules/core/src/index.ts
@@ -1,6 +1,7 @@
 export * from './TCModel';
 export * from './TCString';
 export * from './GVL';
+export * from './Json';
 export * from './model';
 export * from './model/gvl';
 export * from './errors';
