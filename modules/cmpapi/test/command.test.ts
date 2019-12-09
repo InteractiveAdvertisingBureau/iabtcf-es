55e066099cf13716d85803bcc836548ae689347a
Shane Swayney <sswayney@conversantmedia.com>
2019-12-09 11:33:24 -0800

    Created PingCommand tests

--- modules/cmpapi/test/command.test.ts
+++ modules/cmpapi/test/command.test.ts
@@ -1,3 +1,5 @@
 import * as CallbackTests from './command/callback/Callback.test';
+import * as PingCommandTests from './command/commands/PingCommand.test';
 
 CallbackTests.run();
+PingCommandTests.run();
