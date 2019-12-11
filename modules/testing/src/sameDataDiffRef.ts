53975cc6f51dba91c10800bb36967f9320e021b8
Shane Swayney <sswayney@conversantmedia.com>
2019-12-11 15:35:21 -0800

    Added some helpful output for same data different ref

--- modules/testing/src/sameDataDiffRef.ts
+++ modules/testing/src/sameDataDiffRef.ts
@@ -3,7 +3,14 @@ import {isPrimitive} from './isPrimitive';
 
 export const sameDataDiffRef = (obj1: object, obj2: object, objName: string): void => {
 
+  if (obj1 === undefined && obj2 === undefined) {
+
+    return;
+
+  }
+
   expect(typeof obj1, `objects are not both ${objName}s!`).to.equal(typeof obj2);
+
   expect(obj1, 'these are the same reference...').not.to.equal(obj2);
 
   const keySet = new Set<string>(Object.keys(obj1).concat(Object.keys(obj2)));
@@ -11,12 +18,14 @@ export const sameDataDiffRef = (obj1: object, obj2: object, objName: string): vo
   for (const key of keySet) {
 
     if (obj1[key] === undefined && obj2[key] === undefined) {
+
       continue;
+
     }
 
     if (isPrimitive(obj1[key])) {
 
-      expect(obj2[key], `${key} is not equal`).to.equal(obj1[key]);
+      expect(obj2[key], `${objName}:${key} is not equal`).to.equal(obj1[key]);
 
     } else {
 
@@ -26,7 +35,7 @@ export const sameDataDiffRef = (obj1: object, obj2: object, objName: string): vo
 
           if (isPrimitive(value)) {
 
-            expect(obj2[key2], `${key2} is not equal`).to.equal(obj1[key2]);
+            expect(obj2[key2], `${objName}:${key2} is not equal`).to.equal(obj1[key2]);
 
           } else {
 
