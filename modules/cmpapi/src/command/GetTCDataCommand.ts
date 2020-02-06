2c7d639681d7b5de55dca7a89db0f77080013cf5
Chris Paterson <1268493+chrispaterson@users.noreply.github.com>
2020-02-06 10:44:55 -0800

    Adding back GetTCDataCommand.ts -- mac casing issues

---
+++ modules/cmpapi/src/command/GetTCDataCommand.ts
@@ -0,0 +1,58 @@
+import {CmpApiModel} from '../CmpApiModel';
+import {Command} from './Command';
+import {GVL, TCModel} from '@iabtcf/core';
+import {PolyFill} from '@iabtcf/util';
+import {TCDataCallback, Callback} from '../callback';
+import {TCData} from '../response';
+
+export class GetTCDataCommand extends Command {
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  public constructor(callback: Callback, param?: any) {
+
+    super(callback, param);
+
+    new PolyFill();
+
+  }
+
+  protected success(): void {
+
+    const callback = this.callback as TCDataCallback;
+
+    const tcModel = CmpApiModel.tcModel as TCModel;
+
+    if (!tcModel.gvl) {
+
+      tcModel.gvl = new GVL(tcModel.vendorListVersion);
+
+    }
+
+    tcModel.gvl.readyPromise
+      .then(
+        (): void => callback(new TCData(this.param), true),
+        this.fail,
+      )
+      .catch(this.fail);
+
+  }
+
+  protected isValid(): boolean {
+
+    // start with this... if it doesn't apply then they get nothing
+    let retr = CmpApiModel.gdprApplies;
+
+    // if the array is undefined, then we're good
+    if ( retr && this.param !== undefined) {
+
+      // if the array is not undefined and is an array of integers
+      retr = Array.isArray(this.param);
+      retr = (retr && this.param.every((item: number): boolean => Number.isInteger(item)));
+
+    }
+
+    return retr;
+
+  }
+
+}
