72294fc3c709ff8d3748f45c4217ad2ef1571aa3
Chris Paterson <1268493+chrispaterson@users.noreply.github.com>
2020-02-24 17:41:17 -0800

    Adding test case to prove #81

---
+++ modules/cmpapi/test/ReportedIssues.test.ts
@@ -0,0 +1,85 @@
+import {CmpApi} from '../src/';
+import {TCData} from '../src/response';
+import {expect} from 'chai';
+
+// eslint-disable-next-line max-len
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars, no-unused-vars, @typescript-eslint/no-var-requires */
+declare global {
+  interface Window {
+    __tcfapi: (
+      command: any,
+      version: any,
+      callback: (response?: any, success?: any) => void,
+      param?: any
+    ) => void;
+
+  }
+}
+
+const API_FUNCTION_NAME = '__tcfapi';
+const API_VERSION = 2;
+
+describe('Reported github issues', (): void => {
+
+  const removeStub = (): void =>{
+
+    // clean up that junk
+    if (typeof window[API_FUNCTION_NAME] === 'function') {
+
+      delete window[API_FUNCTION_NAME];
+
+    }
+
+    const iframes = document.querySelectorAll('iframe');
+
+    for (let i = 0; i < iframes.length; i++) {
+
+      const frame: HTMLElement = iframes[i];
+
+      if (frame !== null && frame.parentNode) {
+
+        frame.parentNode.removeChild(frame);
+
+      }
+
+    }
+
+  };
+
+  beforeEach((): void => {
+
+    require('@iabtcf/stub')();
+
+  });
+  afterEach((): void => {
+
+    removeStub();
+
+  });
+
+  it('81 Should not throw an exception if TCModel is set to null and getTCData is called', (done: () => void): void => {
+
+    const cmpApi = new CmpApi(100, 105);
+
+    const setTCModelToNull = (): void => {
+
+      cmpApi.tcModel = null;
+
+    };
+
+    const callGetTCData = (): void => {
+
+      window.__tcfapi('getTCData', 2, (tcData: TCData): void => {
+
+        expect(tcData, 'tcData').not.to.be.null;
+
+      });
+
+    };
+
+    expect(setTCModelToNull, 'set TCModel to null').not.to.throw();
+    expect(callGetTCData, 'getTCData').not.to.throw();
+
+  });
+
+});
