6550aa56cff51be8d935b9e2fbba38aacc729e5e
Chris Paterson <1268493+chrispaterson@users.noreply.github.com>
2020-03-18 20:44:18 -0700

    fixes #116 but major refactor

--- modules/cmpapi/src/CmpApiModel.ts
+++ modules/cmpapi/src/CmpApiModel.ts
@@ -1,6 +1,6 @@
 import {CmpStatus, DisplayStatus, EventStatus} from './status';
 import {EventListenerQueue} from './EventListenerQueue';
-import {TCModel, TCString, Vector, PurposeRestrictionVector} from '@iabtcf/core';
+import {TCModel} from '@iabtcf/core';
 
 /**
  * Class holds shareable data across cmp api and provides change event listener for TcModel.
@@ -9,210 +9,30 @@ import {TCModel, TCString, Vector, PurposeRestrictionVector} from '@iabtcf/core'
  */
 export class CmpApiModel {
 
-  public static apiVersion = '2';
-  public static tcfPolicyVersion = 2;
-  public static eventStatus: EventStatus;
+  public static readonly apiVersion = '2';
+  public static readonly tcfPolicyVersion = 2;
+  public static readonly eventQueue = new EventListenerQueue();
+  public static disabled = false;
   public static cmpStatus: CmpStatus = CmpStatus.LOADING;
   public static displayStatus: DisplayStatus = DisplayStatus.HIDDEN;
 
+  public static eventStatus: EventStatus;
   public static cmpId: number;
   public static cmpVersion: number;
   public static gdprApplies: boolean;
-  public static eventQueue = new EventListenerQueue();
-
-  private static disabled_ = false;
-  private static tcModel_: TCModel;
-  private static tcString_: string;
-
-  /**
-   * Returns true if the TcModel has been set
-   */
-  public static get disabled(): boolean {
-
-    return this.disabled_;
-
-  }
-  public static set disabled(bool: boolean) {
-
-    if (bool) {
-
-      this.cmpStatus = CmpStatus.ERROR;
-
-    }
-
-    this.disabled_ = bool;
-
-  }
-
-  public static set uiVisible(bool: boolean) {
-
-    if (bool) {
-
-      this.displayStatus = DisplayStatus.VISIBLE;
-
-    } else {
-
-      this.displayStatus = DisplayStatus.DISABLED;
-
-    }
-
-  }
-
-  public static cacheTCString(encodedStr: string): void {
-
-    this.tcString_ = encodedStr;
-
-  }
-
-  public static set tcString(encodedStr: string) {
-
-    this.tcModel = TCString.decode(encodedStr);
-    this.cacheTCString(encodedStr);
-
-  }
-
-  public static get tcString(): string {
-
-    return this.tcString_;
-
-  }
-
-  /**
-   * Returns the current TcModel
-   * @return {TCModel}
-   */
-  public static get tcModel(): TCModel | null {
-
-    return this.tcModel_;
-
-  }
-
-  /**
-   * @param {TCModel | null} model
-   * @return {void}
-   */
-  public static set tcModel(model: TCModel | null) {
-
-    /**
-     * if this is a TCModel, then we know that GDPRApplies.  Otherwise if they
-     * set explicitly to null then we know that they intend to state that GDPR
-     * does not apply.  If it's something else... Then blowup!
-     */
-
-    if (model === null) {
-
-      this.gdprApplies = false;
-      this.displayStatus = DisplayStatus.DISABLED;
-      this.eventStatus = EventStatus.TC_LOADED;
-      this.tcModel_ = null;
-
-    } else if (this.isTCModel(model)) {
-
-      this.gdprApplies = true;
-
-      switch (this.eventStatus) {
-
-        case undefined:
-          this.eventStatus = EventStatus.TC_LOADED;
-          this.displayStatus = DisplayStatus.HIDDEN;
-          break;
-        case EventStatus.TC_LOADED:
-          this.eventStatus = EventStatus.CMP_UI_SHOWN;
-          this.displayStatus = DisplayStatus.VISIBLE;
-          break;
-        case EventStatus.CMP_UI_SHOWN:
-          this.eventStatus = EventStatus.USER_ACTION_COMPLETE;
-          this.displayStatus = DisplayStatus.HIDDEN;
-          break;
-
-      }
-
-      this.tcModel_ = model.clone();
-      this.tcString_ = '';
-
-    } else {
-
-      this.cmpStatus = CmpStatus.ERROR;
-      throw new Error(`Invalid value (${model}) passed for tcModel`);
-
-    }
-
-    this.cmpStatus = CmpStatus.LOADED;
-    this.eventQueue.exec();
-
-  }
-
-  private static isPurposeRestrictionVector(potentialVector: unknown): potentialVector is PurposeRestrictionVector {
-
-    let retr = false;
-    const subject = potentialVector as PurposeRestrictionVector;
-
-    retr = (typeof subject.add === 'function');
-    retr = (retr && typeof subject.getVendors === 'function');
-    retr = (retr && typeof subject.getRestrictionType === 'function');
-    retr = (retr && typeof subject.vendorHasRestriction === 'function');
-    retr = (retr && typeof subject.getMaxVendorId === 'function');
-    retr = (retr && typeof subject.getRestrictions === 'function');
-    retr = (retr && typeof subject.getPurposes === 'function');
-    retr = (retr && typeof subject.isEmpty === 'function');
-    retr = (retr && typeof subject.isEncodable === 'function');
-
-    return retr;
-
-  }
-
-  private static isVector(potentialVector: unknown): potentialVector is Vector {
-
-    let retr = false;
-    const subject = potentialVector as Vector;
-
-    retr = (typeof subject.maxId === 'number');
-    retr = (retr && typeof subject.has === 'function');
-    retr = (retr && typeof subject.set === 'function');
-    retr = (retr && typeof subject.empty === 'function');
-    retr = (retr && typeof subject.forEach === 'function');
-    retr = (retr && typeof subject.size === 'number');
-    retr = (retr && typeof subject.setAll === 'function');
-
-    return retr;
-
-  }
-
-  private static isTCModel(potentialTCModel: unknown): potentialTCModel is TCModel {
-
-    let retr = false;
-    const subject = potentialTCModel as TCModel;
-    retr = (typeof subject.isServiceSpecific === 'boolean');
-    retr = (retr && typeof subject.useNonStandardStacks === 'boolean');
-    retr = (retr && typeof subject.purposeOneTreatment === 'boolean');
-    retr = (retr && typeof subject.publisherCountryCode === 'string');
-    retr = (retr && this.isVector(subject.vendorsAllowed));
-    retr = (retr && this.isVector(subject.vendorsDisclosed));
-    retr = (retr && this.isVector(subject.purposeConsents));
-    retr = (retr && this.isVector(subject.purposeLegitimateInterests));
-    retr = (retr && this.isVector(subject.vendorConsents));
-    retr = (retr && this.isVector(subject.vendorLegitimateInterests));
-    retr = (retr && this.isVector(subject.specialFeatureOptins));
-    retr = (retr && this.isVector(subject.publisherConsents));
-    retr = (retr && this.isVector(subject.publisherLegitimateInterests));
-    retr = (retr && this.isVector(subject.publisherCustomConsents));
-    retr = (retr && this.isVector(subject.publisherCustomLegitimateInterests));
-    retr = (retr && this.isPurposeRestrictionVector(subject.publisherRestrictions));
-
-    return retr;
-
-  }
+  public static tcModel: TCModel;
+  public static tcString: string;
 
   public static reset(): void {
 
-    delete this.tcModel_;
-    delete this.tcString_;
+    delete this.tcModel;
+    delete this.tcString;
     delete this.cmpId;
     delete this.cmpVersion;
     delete this.gdprApplies;
     delete this.eventStatus;
 
-    this.disabled_ = false;
+    this.disabled = false;
     this.cmpStatus = CmpStatus.LOADING;
     this.displayStatus = DisplayStatus.HIDDEN;
     this.eventQueue.clear();
---
+++ modules/cmpapi/src/PageHandler.ts
@@ -0,0 +1,174 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import {CommandMap} from './command/CommandMap';
+import {CmpApiModel} from './CmpApiModel';
+import {Callback, ErrorCallback} from './callback';
+import {TCFCommands} from './command/TCFCommands';
+import {DisabledCommand} from './command/DisabledCommand';
+import {CustomCommands} from './CustomCommands';
+
+type TcfApiArgs = [string, number, Callback, any];
+type GetQueueFunction = () => TcfApiArgs[];
+type PageCallHandler = (
+  command: string,
+  version: number,
+  callback: (response?: any, success?: any) => void,
+  ...param: any
+) => void;
+
+export class PageHandler {
+
+  private queuedCalls: TcfApiArgs[];
+  private readonly API_FUNCTION_NAME: string = '__tcfapi';
+  private readonly customCommands: CustomCommands;
+
+  public constructor(customCommands?: CustomCommands) {
+
+    this.customCommands = customCommands;
+
+    /**
+     * Attempt to grab the queue – we could call ping and see if it is the stub,
+     * but instead we'll just a feature-detection method of just trying to get
+     * a queue by calling the function with no parameters and see if we get a
+     * queue back. If there is no stub or the stub doesn't return the queue by
+     * calling with no arguments, then we'll just move on and create our
+     * function.
+     */
+    try {
+
+      // get queued commands
+      this.queuedCalls = (window[this.API_FUNCTION_NAME] as GetQueueFunction)();
+
+    } catch (err) {
+
+      // nothing to do here - we tried... no harm no foul
+
+    } finally {
+
+      window[this.API_FUNCTION_NAME] = this.apiCall.bind(this);
+
+    }
+
+  }
+  /**
+   * Handler for all page call commands
+   * @param {string} command
+   * @param {number} version
+   * @param {CallbackFunction} callback
+   * @param {any} [param]
+   */
+  public apiCall(command: string, version: number, callback: Callback, ...params: any): void | never {
+
+    if (typeof command !== 'string') {
+
+      (callback as ErrorCallback)(`invalid command: ${command}`, false);
+
+    } else if (version != 2) {
+
+      /**
+       * Loosely checking version here on purpose.  If a string is passed
+       * that's probably ok, we don't need strict adherence here.
+       */
+
+      (callback as ErrorCallback)(`unsupported version: ${version}`, false);
+
+    } else if (typeof callback !== 'function') {
+
+      throw new Error('invalid callback function');
+
+    } else if (CmpApiModel.disabled) {
+
+      new DisabledCommand(callback);
+
+    } else if (!this.isKnownCommand(command)) {
+
+      /**
+       * This check is here just because the call shouldn't be queued if it's
+       * something we know isn't going to work.  It's kind of like breaking off a bad
+       * relationshipthe instant you know things are not going to work out
+       * instead of letting it linger.
+       */
+
+      (callback as ErrorCallback)(`CmpApi does not support the "${command}" command`, false);
+
+    } else if (command === TCFCommands.PING) {
+
+      /**
+       * if it's a ping we always respond right away regardless of our tcModel
+       * status or other things.
+       */
+      new CommandMap[command](callback, params[0]);
+
+    } else if (CmpApiModel.tcModel === undefined) {
+
+      /**
+       * If we are still waiting for the TC data to be set we can push this
+       * onto the queue that we have and once the model is set it'll be called
+       */
+      this.queuedCalls.push([command, version, callback, params]);
+
+    } else {
+
+      /**
+       * we've passed the checks and we're firing on all cylinders; now lets
+       * get the appropriate command
+       */
+
+      if (this.customCommands && this.customCommands[command]) {
+
+        this.customCommands[command](callback, ...params);
+
+      } else if (CommandMap[command]) {
+
+        new CommandMap[command](callback, params[0]);
+
+      } else {
+
+        // hopefully this isn't possible
+        throw new Error('unknown error');
+
+      }
+
+    }
+
+  }
+  private purgeQueuedCalls(): void {
+
+    if (this.queuedCalls) {
+
+      const apiCall = this.apiCall.bind(this);
+      this.queuedCalls.forEach((args: TcfApiArgs): void =>{
+
+        const [command, version, callback, params] = args;
+
+        if (params !== undefined) {
+
+          apiCall(command, version, callback, ...params);
+
+        } else {
+
+          apiCall(command, version, callback);
+
+        }
+
+      });
+
+      delete this.queuedCalls;
+
+    }
+
+  }
+  /**
+   * Checks to see if the command exists in either the set of TCF Commands or
+   * if custom commands
+   *
+   * @param {string} command - command to check
+   * @return {boolean} - whether or not this command is known
+   */
+  private isKnownCommand(command: string): boolean {
+
+    return ((this.customCommands !== undefined && this.customCommands[command] !== undefined) ||
+      (CommandMap[command] !== undefined));
+
+  }
+
+}
