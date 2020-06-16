b0a4b6b829e9788388b26b84b3cc0f7f165b1cc0
Chris Paterson <1268493+chrispaterson@users.noreply.github.com>
2020-06-16 14:22:35 -0700

    More work for #166

--- modules/stub/src/stub.js
+++ modules/stub/src/stub.js
@@ -1,241 +1,282 @@
 ((win, doc) => {
 
+  const TCF_API_NAME = '__tcfapi';
+  const POST_MESSAGE_EVENT = 'message';
+  const TCF_POST_MSG_CALL = TCF_API_NAME + 'Call';
+  const TCF_POST_MSG_RETURN = TCF_API_NAME + 'Return';
+  const TCF_LOCATOR_NAME = TCF_API_NAME + 'Locator';
+  const CMP_STATUS = 'stub';
+  const TARGET_ORIGIN = '*';
+  const PING_COMMAND = 'ping';
+  const GDPR_APPLIES_COMMAND = 'setGdprApplies';
+
   const makeStub = () => {
 
-    const TCF_API_NAME = '__tcfapi';
-    const EVENT_NAME = 'message';
-    const TCF_POST_MSG_CALL = TCF_API_NAME + 'Call';
-    const TCF_POST_MSG_RETURN = TCF_API_NAME + 'Return';
-    const TCF_LOCATOR_NAME = TCF_API_NAME + 'Locator';
-    const CMP_STATUS = 'stub';
-    const TARGET_ORIGIN = '*';
-    const PING_COMMAND = 'ping';
-    const GDPR_APPLIES_COMMAND = 'setGdprApplies';
+    const isLeader = (win === win.top && win[TCF_API_NAME] === undefined);
 
-    if (!(win.frames[TCF_LOCATOR_NAME])) {
+    const getPoxyingMethod = () => {
 
-      if (doc.body) {
+      const callMap = new Map();
+      let callId = 0;
 
-        const iframe = doc.createElement('iframe');
+      return (command, version, callback, parameter) => {
 
-        /**
-         * Create the locator frame in this frame since we've established that
-         * none exists in this frame.  This is to prevent instances of this
-         * script from overriding each other.  If this one queues calls and
-         * gets overridden then the queue is lost and calls are dropped.
-         */
-        iframe.style.cssText = 'display:none';
-        iframe.name = TCF_LOCATOR_NAME;
-        doc.body.appendChild(iframe);
+        const call ={
+          callback,
+          command,
+          version,
+          parameter,
+        };
 
-        if (win === win.top || !(win.parent.frames[TCF_LOCATOR_NAME])) {
+        callMap.set(callId, call);
+
+        win.top.postMessage({
+          [TCF_POST_MSG_CALL]: {
+            command,
+            parameter,
+            version,
+            callId,
+          },
+        }, TARGET_ORIGIN);
+
+        callId++;
+
+      };
+
+    };
+
+    const getQueingMethod = () => {
+
+      const queue = [];
+      let gdprApplies;
+
+      return (command, version, callback, parameter) => {
+
+        const hasCallback = (typeof callback === 'function');
+
+        if (!command) {
 
           /**
-           * There are no parent stubs or CMPs, so it falls on this instance to
-           * collect calls in a queue
+           * shortcut to get the queue when the full CMP
+           * implementation loads; it can call __tcfapi
+           * with no arguments to get the queued arguments
            */
 
-          const queue = [];
+          return queue;
 
-          win[TCF_API_NAME] = (command, version, callback, parameter) => {
+        } else if (command === GDPR_APPLIES_COMMAND) {
 
-            let gdprApplies;
+          /**
+           * Most CMPs upon load will have some form of determining whether
+           * or not GDPR Applies to this particular user and so this value
+           * generally can't be known before the CMP loads.  However, there
+           * are cases where a site knows that they need to apply GDPR to
+           * all traffic; like if the site is based in the EU.  This command
+           * enables that value to be set upon the stub response.
+           *
+           * This command is not specified in the TCF and so no callback is
+           * necessary and therefor is optional.
+           */
 
-            if (!command) {
+          gdprApplies = parameter;
 
-              /**
-               * shortcut to get the queue when the full CMP
-               * implementation loads; it can call __tcfapi
-               * with no arguments to get the queued arguments
-               */
+          if (hasCallback) {
 
-              return queue;
+            callback(null, true);
 
-            } else if (command === GDPR_APPLIES_COMMAND) {
+          }
 
-              /**
-               * shortcut to set gdprApplies if the publisher
-               * knows that they apply GDPR rules to all
-               * traffic (see the section on "What does the
-               * gdprApplies value mean" for more
-               */
+        } else if (command === PING_COMMAND && hasCallback) {
 
-              gdprApplies = parameter;
+          /**
+             * This is the only method required from a stub.
+             */
 
-              if (callback) {
-
-                callback(null, true);
-
-              }
-
-            } else if (command === PING_COMMAND) {
-
-              /**
-               * Only supported method; give PingReturn
-               * object as response
-               */
-
-              if (callback) {
-
-                callback({
-                  gdprApplies,
-                  cmpLoaded: false,
-                  cmpStatus: CMP_STATUS,
-                }, true);
-
-              }
-
-            } else {
-
-              /**
-               * some other method, just queue it for the
-               * full CMP implementation to deal with
-               */
-
-              queue.push([command, version, callback, parameter]);
-
-            }
-
-          };
-
-          const postMessageEventHandler = (event) => {
-
-            if (event && event.source && event.source.postMessage) {
-
-              const msgIsString = typeof event.data === 'string';
-              let json = {};
-
-              if (msgIsString) {
-
-                try {
-
-                  /**
-                   * Try to parse the data from the event.  It is important to
-                   * have in a try/catch because often string messages may come
-                   * through that are not json
-                   */
-
-                  json = JSON.parse(event.data);
-
-                } catch (ignore) {/* oh well... */}
-
-              } else {
-
-                json = event.data;
-
-              }
-
-              const payload = json[TCF_POST_MSG_CALL];
-
-              if (payload) {
-
-                const {command, version, callId, parameter} = payload;
-
-                const wrapperCallback = (returnValue, success) => {
-
-                  let returnMsg = {
-                    [TCF_POST_MSG_RETURN]: {
-                      returnValue,
-                      success,
-                      callId,
-                    },
-                  };
-
-                  if (msgIsString) {
-
-                    /**
-                     * If it came in as a string it will be return in the same
-                     * manner
-                     */
-
-                    returnMsg = JSON.stringify(returnMsg);
-
-                  }
-
-                  event.source.postMessage(returnMsg, TARGET_ORIGIN);
-
-                };
-
-                win[TCF_API_NAME](
-                  command,
-                  version,
-                  wrapperCallback,
-                  parameter,
-                );
-
-              }
-
-            }
-
-          };
-
-          win.addEventListener(EVENT_NAME, postMessageEventHandler);
+          callback({
+            gdprApplies,
+            cmpLoaded: false,
+            cmpStatus: CMP_STATUS,
+          }, true);
 
         } else {
 
-          let callId = 0;
-          const callMap = new Map();
+          /**
+             * some other method, just queue it for the
+             * full CMP implementation to deal with
+             */
 
-          win[TCF_API_NAME] = (command, version, callback, parameter) => {
-
-            callMap.set(callId, callback);
-
-            const msg = {
-              [TCF_POST_MSG_CALL]: {
-                command,
-                parameter,
-                version,
-                callId,
-              },
-            };
-
-            win.parent.postMessage(msg, TARGET_ORIGIN);
-            callId++;
-
-          };
-
-          const responseHandler = () => {
-
-            let json;
-
-            if (typeof event.data === 'string') {
-
-              try {
-
-                json = JSON.parse(event.data);
-
-              } catch (ignore) {}
-
-            } else if ( typeof event.data === 'object') {
-
-              json = event.data;
-
-            }
-
-            const payload = json[TCF_POST_MSG_RETURN];
-
-            if (payload && callMap.has(payload.callId)) {
-
-              const callback = callMap.get(payload.callId);
-
-              callback(payload.returnValue, payload.success);
-              callMap.delete(payload.callId);
-
-            }
-
-          };
-
-          win.addEventListener(EVENT_NAME, responseHandler, false);
+          queue.push([command, version, callback, parameter]);
 
         }
 
-      } else {
+      };
 
-        setTimeout(makeStub, 5);
+    };
+
+    /**
+     * if this instance is running in window.top, then that is where a CMP
+     * should be created and therefore where the queue should exist.  If not,
+     * this stub exists likely as part of Ad Code inside of an iframe and
+     * should then act as a proxy for calls from the Ad Code to the CMP.
+     */
+
+    if (isLeader) {
+
+      win[TCF_API_NAME] = getQueingMethod();
+
+    } else {
+
+      win[TCF_API_NAME] = getPoxyingMethod();
+
+    }
+
+    /**
+     * Create the locator frame if one doesn't exist here (it shouldn't).  With
+     * this, we have to wait for the body to exist; for the rest of this script
+     * we do not want to do that because we will miss any calls that are
+     * placed in the head of the document.
+     */
+
+    const addLocatorFrame = () => {
+
+      if (!win.frames[TCF_LOCATOR_NAME]) {
+
+        if (doc.body) {
+
+          const iframe = doc.createElement('iframe');
+
+          iframe.style.cssText = 'display:none';
+          iframe.name = TCF_LOCATOR_NAME;
+          doc.body.appendChild(iframe);
+
+        } else {
+
+          setTimeout(addLocatorFrame, 5);
+
+        }
 
       }
 
-    }
+    };
+
+    addLocatorFrame();
+
+    /**
+     * Listen for postMessage calls
+     */
+
+    const callMap = new Map();
+    const addEventListenerMap = new Map();
+
+    win.addEventListener(POST_MESSAGE_EVENT, (event) => {
+
+      /**
+       * Make sure an event with a source and date and way to respond are
+       * available before doing anything with it.
+       */
+
+      if (event && event.data) {
+
+        if (typeof event.data === 'string') {
+
+          try {
+
+            /**
+             * Try to parse the data from the event; there is no guarantee
+             * that this string is a stringified object – in fact it's
+             * probably more likely that it's not.
+             */
+
+            event.data = JSON.parse(event.data);
+
+          } catch (ignore) {/* oh well... */}
+
+        }
+
+        if (typeof event.data === 'object') {
+
+          if (isLeader && event.data[TCF_POST_MSG_CALL]) {
+
+            /**
+             * Since this instance is at the top, then the CMP exists here
+             * and we'll wrap all postMessage callbacks. Otherwise, this
+             * message should be ignored because this instance isn't tasked
+             * with handling it.
+             */
+
+            const {command, version, callId, parameter} = event.data[TCF_POST_MSG_CALL];
+
+            const wrapperCallback = (returnValue, success) => {
+
+              if (event.source && event.source.postMessage) {
+
+                event.source.postMessage({
+                  [TCF_POST_MSG_RETURN]: {
+                    returnValue,
+                    success,
+                    callId,
+                  },
+                }, TARGET_ORIGIN);
+
+              }
+
+            };
+
+            win[TCF_API_NAME](
+              command,
+              version,
+              wrapperCallback,
+              parameter,
+            );
+
+          } else if (!isLeader && event.data[TCF_POST_MSG_RETURN]) {
+
+            const {callId, returnValue, success} = event.data[TCF_POST_MSG_RETURN];
+
+            if (callMap.has(callId)) {
+
+              const call = callMap.get(callId);
+
+              /**
+               * Because an addEventListener command may have the callback
+               * invoked more than once, we can not remove the mapping in the
+               * callmap on that command.  Instead, when this is called
+               * we'll store a map between the listenerId returned from the
+               * CMP and the callId.  Later when the removeEventListener
+               * command is called, the mapping will be used to delete entry
+               * in the map.  If it's neither of those commands just delete
+               * the mapping.
+               */
+              if (call.command == 'addEventListener') {
+
+                addEventListenerMap.set(returnValue.listenerId, callId);
+
+              } else {
+
+                if (call.command == 'removeEventListener') {
+
+                  const aelCallId = addEventListenerMap.get(call.parameter);
+
+                  callMap.delete(aelCallId);
+                  addEventListenerMap.delete(call.parameter);
+
+                }
+
+                callMap.delete(payload.callId);
+
+              }
+
+            }
+
+            call.callback(returnValue, success);
+
+          }
+
+        }
+
+      }
+
+    });
 
   };
 
