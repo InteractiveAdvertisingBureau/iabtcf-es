4badba0a9f1f9dcd58c3532e8a0a2e0fcaca87f9
Sergei Sevriugin <sevriugin@gmail.com>
2023-08-01 19:49:33 +0200

    fix library entry points

--- modules/core/test/model/gvl/DataCategory.test.ts
+++ modules/core/test/model/gvl/DataCategory.test.ts
@@ -2,7 +2,7 @@ import {expect} from 'chai';
 import {DataCategory} from '../../../src/model/gvl/DataCategory';
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
-const vendorlistJson = require('@iabtcf/testing/lib/vendorlist/v2.2/vendor-list.json');
+const vendorlistJson = require('@iabtechlabtcf/testing/lib/vendorlist/v2.2/vendor-list.json');
 
 export function run(): void {
 
